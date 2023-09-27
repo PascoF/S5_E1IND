b0f2af15596aa22c128ec8b07d88e65eb5d5e32a
PascoF <113253733+PascoF@users.noreply.github.com>
2023-09-26 21:02:45 -0500

    FrutosEnPlantas

---
+++ ExamenIndividual/Frutos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenIndividual
+{
+    class Frutos
+    {
+        private string nombre;
+        private float cantidaddeproducto;
+
+        public Frutos(string nombre, float cantidaddeproducto)
+        {
+            this.nombre = nombre;
+            this.cantidaddeproducto = cantidaddeproducto;
+        }
+
+        public float GetCantProducto()
+        {
+            return cantidaddeproducto;
+        }
+
+        public string GetName()
+        {
+            return nombre;
+        }
+}
+}
