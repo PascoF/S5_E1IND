e5ad9bb72d3d4d4307bf27621ce24c08284c956e
PascoF <113253733+PascoF@users.noreply.github.com>
2023-09-26 21:20:37 -0500

    Granja_Lista

---
+++ ExamenIndividual/Granja.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenIndividual
+{
+    class Granja
+    {
+        public void Acciones()
+        {
+            //Lista
+            Frutos Manzana = new Frutos("Manzana", 3);
+            Frutos Naranja = new Frutos("Naranja", 4);
+            Frutos Fresas = new Frutos("Frutos", 9);
+            Arboles Manzanos = new Arboles("Manzano", 12, 14, 35, Manzana);
+            Arboles Naranjos = new Arboles("Naranjo", 15, 16, 24, Naranja);
+
+
+        }
+    }
+}
