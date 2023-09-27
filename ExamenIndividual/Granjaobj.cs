70e5ff30447ad550e3330a3a8a978c61471435f3
PascoF <113253733+PascoF@users.noreply.github.com>
2023-09-26 20:04:28 -0500

    ContGranja

---
+++ ExamenIndividual/Granjaobj.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenIndividual
+{
+    class Granjaobj
+    {
+
+        //Contenido de la granja: plantas y animales
+
+        protected string nombre;
+        protected int precioinicial;
+        protected int preciofinal;
+
+        protected Granjaobj(string nombre, int precioinicial, int preciofinal)
+        {
+            this.nombre = nombre;
+            this.precioinicial = precioinicial;
+            this.preciofinal = preciofinal;
+
+        }
+        public string GetName()
+        {
+            return nombre;
+        }
+    }
+}
